15ddb99d25fd945118379999110916ebbd9c1adb
kagulion <kagulion@vk.com>
2026-01-06 13:46:35 +0300

    update: header info

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Madfilms";
-export const SITE_DESCRIPTION =
+export const SITE_DESCRIPTION = "Фильмы с комментариями Ильи Мэддисона";
+export const SITE_ABOUT =
   "Все фильмы, которые посмотрел Илья Мэддисон на своих стримах";
 export const TELEGRAM_LINK = "https://t.me/maddysonfilms";
