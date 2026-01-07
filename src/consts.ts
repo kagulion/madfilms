f03c99a13cd75fec82820632ad45f453db6e5133
kagulion <kagulion@vk.com>
2026-01-07 18:07:15 +0300

    add: site version

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_VERSION = "1.0";
 export const SITE_TITLE = "Madfilms";
 export const SITE_FULL_TITLE = "Maddyson Films";
 export const SITE_DESCRIPTION = "Фильмы с комментариями Ильи Мэддисона";
