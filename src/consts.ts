8c5fd1d5fedef01b45ff29fd08029d3e728b7036
kagulion <kagulion@vk.com>
2026-01-12 22:19:11 +0300

    update: header style

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,11 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+// Данные сайта
 export const SITE_VERSION = "1.0";
 export const SITE_TITLE = "Madfilms";
 export const SITE_FULL_TITLE = "Maddyson Films";
 export const SITE_DESCRIPTION = "Фильмы с комментариями Ильи Мэддисона";
 export const SITE_ABOUT =
   "Все фильмы, которые посмотрел Илья Мэддисон на своих стримах";
+
+// Ссылки
 export const TELEGRAM_LINK = "https://t.me/maddysonfilms";
+export const VKVIDEO_LINK = "https://vkvideo.ru/@maddysonfilms";
